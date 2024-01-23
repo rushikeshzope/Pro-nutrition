75f80272bec04e255b0c394631b0f5e7768c4d6d
rushikeshzope <rushikesh.zope@gmail.com>
2024-01-23 14:48:50 +0530

    push

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { Component } from "react";
+import './App.css';
+import FoodData from './resources/FoodData';
+import FoodBox from './components/Foodbox';
+
+export default class App extends Component{
+
+  constructor(){
+    super();
+    this.state = {
+      filteredData : [...FoodData],
+    }
+  }
+
+  render() {
+
+    let handleChange = (event) => {
+      let searchText = event.target.value.toLowerCase();
+
+      let searchResults = FoodData.filter((element) => {
+        return element.name.includes(searchText);
+      })
+
+      this.setState({
+        filteredData : searchResults,
+      });
+    }
+
+    return(
+      <>
+        <div className="input">
+          <input className="search-inp" type="text" placeholder="Search Here" onChange={handleChange} />
+        </div>
+
+        {this.state.filteredData.map((element, i) => (
+          <FoodBox foodItem={element} key={i} />
+        ))
+        
+        }
+      </>
+    )
+  }
+}
